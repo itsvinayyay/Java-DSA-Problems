2983080aabd71276b4352e5f631537e2b83986f9
itsvinayyayBIB <vinay.v@buildinbharat.com>
2024-10-07 18:58:30 +0530

    Java DSA Problems

--- 11.Bitwise/Example/PowerOfTwo.java
+++
@@ -1,7 +0,0 @@
-public class PowerOfTwo {
-    public static void main(String[] args) {
-        int n = 31; // note: fix for n = 0
-        boolean ans = (n & (n-1)) == 0;
-        System.out.println(ans);
-    }
-}
